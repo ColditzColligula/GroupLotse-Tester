2160b311ab20a9953d24842b9596bb796bd5f8f5
Colditz Colligula <79027579+ColditzColligula@users.noreply.github.com>
2022-05-20 00:06:47 +0200

    Add files via upload

--- Grouplotse.py
+++ Grouplotse.py
@@ -3,55 +3,60 @@ import random
 import requests
 import json
 
+
 class Grouplotse:
-    #attributes#methods
+    # attributes#methods
     def webhook_file_post(self, webhook_addr, webhook_key, filepath, file_extension):
         """Main Function to send images via Webhook.
         webhook_addr is the URL of your Webhook. webhook_key is the Key.
         Next, specify the file and pass the file extension."""
         try:
             payload = {}
-            files = [
-                ('file', (f'data{file_extension}', open(f'{filepath}', 'rb')))
-            ]
+            files = [("file", (f"data{file_extension}", open(f"{filepath}", "rb")))]
             headers = {
-                'Authorization': webhook_key,
+                "Authorization": webhook_key,
             }
             requests.post(webhook_addr, headers=headers, data=payload, files=files)
             print(f'Sending "{filepath}" as POST Message to your Grouplotse')
         except:
-            raise Exception("Failed to send message. Key or Webhook Address might be incorrect or your GroupLotse isn't"
-                            "configured correctly!")
-
+            raise Exception(
+                "Failed to send message. Key or Webhook Address might be incorrect or your GroupLotse isn't"
+                "configured correctly!"
+            )
 
     def webhook_post(self, webhook_addr, webhook_key, webhook_msg):
         """Main function to send Text via Webhook.
         webhook_addr is the URL of your Webhook. webhook_key is the Key.
         Pass a String to webhook_msg and call the method."""
-        webhook_lotse = (webhook_addr+"?key="+webhook_key)
+        webhook_lotse = webhook_addr + "?key=" + webhook_key
 
         try:
-             requests.post(webhook_lotse, data=json.dumps(webhook_msg), headers={'Content-Type': 'application/json'})
-             print(f'Sending "{webhook_msg}" as POST Message to your Grouplotse')
+            requests.post(
+                webhook_lotse,
+                data=json.dumps(webhook_msg),
+                headers={"Content-Type": "application/json"},
+            )
+            print(f'Sending "{webhook_msg}" as POST Message to your Grouplotse')
         except:
-            raise Exception("Failed to send message. Key or Webhook Address might be incorrect or your GroupLotse isn't"
-                            "configured correctly!")
-
+            raise Exception(
+                "Failed to send message. Key or Webhook Address might be incorrect or your GroupLotse isn't"
+                "configured correctly!"
+            )
 
     def mqtt_send(self, topic, username, password, mqttmsg):
         """Main function to send Text message via MQTT.
         The client_id and broker are set to the standard for Grouplotse. Make sure you pass
         the right topic, username and password - followed by your String for 'mqttmsg' and call
         the method to send."""
-        client_id = f'python-mqtt-{random.randint(0, 1000)}'
+        client_id = f"python-mqtt-{random.randint(0, 1000)}"
         broker = "mqtt.grouplotse.com"
         port = 1883
+
         def on_connect(client, userdata, flags, rc):
             if rc == 0:
                 print("Connected to MQTT Broker!")
             else:
                 print("Failed to connect, return code %d\n", rc)
-
 
         # Set Connecting Client ID
         client = mqtt_client.Client(client_id)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 import os
 from Grouplotse import Grouplotse
 import webbrowser
+
 sg.theme("LightBrown11")
 
 
@@ -10,8 +11,7 @@ def main():
     iswebhookFile = os.path.isfile(webhookfile)
     filepath = ""
     mqttfile = "grouplotse_tester_mqtt.txt"
-    ismqttFile= os.path.isfile(mqttfile)
-
+    ismqttFile = os.path.isfile(mqttfile)
 
     if not iswebhookFile:
         with open("grouplotse_tester_webhook.txt", "w") as glsetwebhook:
@@ -21,9 +21,23 @@ def main():
         webhooklayout = [
             [sg.Text("Webhook_URL:"), sg.Push(), sg.Input("", key="-WEBHOOK-")],
             [sg.Text("Webhook_Key:"), sg.Push(), sg.Input("", key="-KEY-")],
-            [sg.Text("Message:"), sg.Push(), sg.Multiline(key="-WHMESSAGE-", size=(50, 6))],
-            [sg.FileBrowse("Browse", key="-FILEBROWSE-"), sg.Push(), sg.InputText(filepath, key="-FILEPATHWH-", do_not_clear=False)],
-            [sg.Button("Send", key="-WEBHOOKSEND-"), sg.Button("Exit", key="-EXIT0-"), sg.Push(), sg.Text("Max Filesize: 1024Kb"), sg.Push()],
+            [
+                sg.Text("Message:"),
+                sg.Push(),
+                sg.Multiline(key="-WHMESSAGE-", size=(50, 6)),
+            ],
+            [
+                sg.FileBrowse("Browse", key="-FILEBROWSE-"),
+                sg.Push(),
+                sg.InputText(filepath, key="-FILEPATHWH-", do_not_clear=False),
+            ],
+            [
+                sg.Button("Send", key="-WEBHOOKSEND-"),
+                sg.Button("Exit", key="-EXIT0-"),
+                sg.Push(),
+                sg.Text("Max Filesize: 1024Kb"),
+                sg.Push(),
+            ],
         ]
 
     if iswebhookFile:
@@ -34,9 +48,23 @@ def main():
                 webhooklayout = [
                     [sg.Text("Webhook_URL:"), sg.Push(), sg.Input("", key="-WEBHOOK-")],
                     [sg.Text("Webhook_Key:"), sg.Push(), sg.Input("", key="-KEY-")],
-                    [sg.Text("Message:"), sg.Push(), sg.Multiline(key="-WHMESSAGE-", size=(50, 6))],
-                    [sg.FileBrowse("Browse", key="-FILEBROWSE-"), sg.Push(), sg.InputText(filepath, key="-FILEPATHWH-", do_not_clear=False)],
-                    [sg.Button("Send", key="-WEBHOOKSEND-"), sg.Button("Exit", key="-EXIT0-"), sg.Push(), sg.Text("Max Filesize: 1024Kb"), sg.Push()],
+                    [
+                        sg.Text("Message:"),
+                        sg.Push(),
+                        sg.Multiline(key="-WHMESSAGE-", size=(50, 6)),
+                    ],
+                    [
+                        sg.FileBrowse("Browse", key="-FILEBROWSE-"),
+                        sg.Push(),
+                        sg.InputText(filepath, key="-FILEPATHWH-", do_not_clear=False),
+                    ],
+                    [
+                        sg.Button("Send", key="-WEBHOOKSEND-"),
+                        sg.Button("Exit", key="-EXIT0-"),
+                        sg.Push(),
+                        sg.Text("Max Filesize: 1024Kb"),
+                        sg.Push(),
+                    ],
                 ]
             else:
                 split_list = fileread.split("#")
@@ -44,18 +72,34 @@ def main():
                 webhook_key = split_list[1]
                 print("Layout Filled")
                 webhooklayout = [
-                    [sg.Text("Webhook_URL:"), sg.Push(), sg.Input(webhook_split, key="-WEBHOOK-")],
-                    [sg.Text("Webhook_Key:"), sg.Push(), sg.Input(webhook_key, key="-KEY-")],
-                    [sg.Text("Message:"), sg.Push(), sg.Multiline(key="-WHMESSAGE-", size=(50, 6))],
-                    [sg.FileBrowse("Browse", key="-FILEBROWSE-"), sg.Push(), sg.InputText(filepath, key="-FILEPATHWH-", do_not_clear=False)],
-                    [sg.Button("Send", key="-WEBHOOKSEND-"), sg.Button("Exit", key="-EXIT0-"), sg.Push(), sg.Text("Max Filesize: 1024Kb"), sg.Push()],
+                    [
+                        sg.Text("Webhook_URL:"),
+                        sg.Push(),
+                        sg.Input(webhook_split, key="-WEBHOOK-"),
+                    ],
+                    [
+                        sg.Text("Webhook_Key:"),
+                        sg.Push(),
+                        sg.Input(webhook_key, key="-KEY-"),
+                    ],
+                    [
+                        sg.Text("Message:"),
+                        sg.Push(),
+                        sg.Multiline(key="-WHMESSAGE-", size=(50, 6)),
+                    ],
+                    [
+                        sg.FileBrowse("Browse", key="-FILEBROWSE-"),
+                        sg.Push(),
+                        sg.InputText(filepath, key="-FILEPATHWH-", do_not_clear=False),
+                    ],
+                    [
+                        sg.Button("Send", key="-WEBHOOKSEND-"),
+                        sg.Button("Exit", key="-EXIT0-"),
+                        sg.Push(),
+                        sg.Text("Max Filesize: 1024Kb"),
+                        sg.Push(),
+                    ],
                 ]
-
-
-
-
-
-
 
     if not ismqttFile:
         with open("grouplotse_tester_mqtt.txt", "w") as glsetmqtt:
@@ -63,13 +107,21 @@ def main():
             glsetmqtt.close
         print("Layout Empty")
         mqttlayout = [
-            [sg.Text("Server URL:"), sg.Push(), sg.Input("http://mqtt.grouplotse.com", key="-MQTTBROKER-")],
+            [
+                sg.Text("Server URL:"),
+                sg.Push(),
+                sg.Input("http://mqtt.grouplotse.com", key="-MQTTBROKER-"),
+            ],
             [sg.Text("Port:"), sg.Push(), sg.Input("1883", key="-MQTTPORT-")],
             [sg.Text("Topic:"), sg.Push(), sg.Input("", key="-MQTTTOPIC-")],
             [sg.Text("Username:"), sg.Push(), sg.Input("", key="-MQTTUSERNAME-")],
             [sg.Text("Password:"), sg.Push(), sg.Input("", key="-MQTTPASSWORD-")],
-            [sg.Text("Message:"), sg.Push(), sg.Multiline(key="-MQTTMESSAGE-", size=(50, 6))],
-            [sg.Button("Send", key="-MQTTSEND-"), sg.Button("Exit", key="-EXIT1-")]
+            [
+                sg.Text("Message:"),
+                sg.Push(),
+                sg.Multiline(key="-MQTTMESSAGE-", size=(50, 6)),
+            ],
+            [sg.Button("Send", key="-MQTTSEND-"), sg.Button("Exit", key="-EXIT1-")],
         ]
 
     if ismqttFile:
@@ -78,13 +130,32 @@ def main():
             if not fileread:
                 pass
                 mqttlayout = [
-                    [sg.Text("Server URL:"), sg.Push(), sg.Input("http://mqtt.grouplotse.com", key="-MQTTBROKER-")],
+                    [
+                        sg.Text("Server URL:"),
+                        sg.Push(),
+                        sg.Input("http://mqtt.grouplotse.com", key="-MQTTBROKER-"),
+                    ],
                     [sg.Text("Port:"), sg.Push(), sg.Input("1883", key="-MQTTPORT-")],
                     [sg.Text("Topic:"), sg.Push(), sg.Input("", key="-MQTTTOPIC-")],
-                    [sg.Text("Username:"), sg.Push(), sg.Input("", key="-MQTTUSERNAME-")],
-                    [sg.Text("Password:"), sg.Push(), sg.Input("", key="-MQTTPASSWORD-")],
-                    [sg.Text("Message:"), sg.Push(), sg.Multiline(key="-MQTTMESSAGE-", size=(50, 6))],
-                    [sg.Button("Send", key="-MQTTSEND-"), sg.Button("Exit", key="-EXIT1-")]
+                    [
+                        sg.Text("Username:"),
+                        sg.Push(),
+                        sg.Input("", key="-MQTTUSERNAME-"),
+                    ],
+                    [
+                        sg.Text("Password:"),
+                        sg.Push(),
+                        sg.Input("", key="-MQTTPASSWORD-"),
+                    ],
+                    [
+                        sg.Text("Message:"),
+                        sg.Push(),
+                        sg.Multiline(key="-MQTTMESSAGE-", size=(50, 6)),
+                    ],
+                    [
+                        sg.Button("Send", key="-MQTTSEND-"),
+                        sg.Button("Exit", key="-EXIT1-"),
+                    ],
                 ]
             else:
                 split_list = fileread.split("#")
@@ -93,38 +164,64 @@ def main():
                 password = split_list[4]
                 print("Layout Filled")
                 mqttlayout = [
-                    [sg.Text("Server URL:"), sg.Push(), sg.Input("mqtt.grouplotse.com", key="-MQTTBROKER-")],
+                    [
+                        sg.Text("Server URL:"),
+                        sg.Push(),
+                        sg.Input("mqtt.grouplotse.com", key="-MQTTBROKER-"),
+                    ],
                     [sg.Text("Port:"), sg.Push(), sg.Input("1883", key="-MQTTPORT-")],
                     [sg.Text("Topic:"), sg.Push(), sg.Input(topic, key="-MQTTTOPIC-")],
-                    [sg.Text("Username:"), sg.Push(), sg.Input(username, key="-MQTTUSERNAME-")],
-                    [sg.Text("Password:"), sg.Push(), sg.Input(password, key="-MQTTPASSWORD-")],
-                    [sg.Text("Message:"), sg.Push(), sg.Multiline(key="-MQTTMESSAGE-", size=(50, 6))],
-                    [sg.Button("Send", key="-MQTTSEND-"), sg.Button("Exit", key="-EXIT1-")]
+                    [
+                        sg.Text("Username:"),
+                        sg.Push(),
+                        sg.Input(username, key="-MQTTUSERNAME-"),
+                    ],
+                    [
+                        sg.Text("Password:"),
+                        sg.Push(),
+                        sg.Input(password, key="-MQTTPASSWORD-"),
+                    ],
+                    [
+                        sg.Text("Message:"),
+                        sg.Push(),
+                        sg.Multiline(key="-MQTTMESSAGE-", size=(50, 6)),
+                    ],
+                    [
+                        sg.Button("Send", key="-MQTTSEND-"),
+                        sg.Button("Exit", key="-EXIT1-"),
+                    ],
                 ]
 
-
-    aboutlayout = [[sg.Multiline("GroupLotse is a virtual assistant that enables quick clarification of responsibilities\n"
-                           " and precise device control. Your team can be actively involved in all decision-making\n"
-                   " processes. Furthermore, it is also possible to fully automate all processes.\n"
-                    "\n\nYour team can interact with GroupLotse via the common communication and collaboration\n"
-                   " platforms Slack, Microsoft Teams and Telegram. This ensures that GroupLotse can be productively\n"
-                   " integrated into workflows and that all relevant information is available at all times. To achieve\n"
-                   " this, GroupLotse is simply integrated into your existing channels and chat groups.\n",
-                            expand_y=True), sg.Button("", image_filename="gl_icon.png", key='-WEBSITEBUTTON-')]]
-
+    aboutlayout = [
+        [
+            sg.Multiline(
+                "GroupLotse is a virtual assistant that enables quick clarification of responsibilities\n"
+                " and precise device control. Your team can be actively involved in all decision-making\n"
+                " processes. Furthermore, it is also possible to fully automate all processes.\n"
+                "\n\nYour team can interact with GroupLotse via the common communication and collaboration\n"
+                " platforms Slack, Microsoft Teams and Telegram. This ensures that GroupLotse can be productively\n"
+                " integrated into workflows and that all relevant information is available at all times. To achieve\n"
+                " this, GroupLotse is simply integrated into your existing channels and chat groups.\n",
+                expand_y=True,
+            ),
+            sg.Button("", image_filename="gl_icon.png", key="-WEBSITEBUTTON-"),
+        ]
+    ]
 
     #   Tab Group Layout (must contain ONLY tabs)
-    tab_group_layout = [[sg.Tab('Webhook', webhooklayout, key='-TAB0-'),
-                            sg.Tab('MQTT', mqttlayout, key='-TAB1-'),
-                         sg.Tab('About', aboutlayout, key='-TABAB-')
-                            ]]
+    tab_group_layout = [
+        [
+            sg.Tab("Webhook", webhooklayout, key="-TAB0-"),
+            sg.Tab("MQTT", mqttlayout, key="-TAB1-"),
+            sg.Tab("About", aboutlayout, key="-TABAB-"),
+        ]
+    ]
 
     #   The window layout - defines the entire window
-    layout = [[sg.Image("gl_logo_banner.png")],
-              [sg.TabGroup(tab_group_layout,
-                        enable_events=True,
-                        key='-TABGROUP-')]]
-
+    layout = [
+        [sg.Image("gl_logo_banner.png")],
+        [sg.TabGroup(tab_group_layout, enable_events=True, key="-TABGROUP-")],
+    ]
 
     window = sg.Window("GroupLotse Tester", layout)
 
@@ -138,7 +235,6 @@ def main():
         filepath = values["-FILEBROWSE-"]
         window["-FILEPATHWH-"].update(filepath)
 
-
         if event == "-WEBHOOKSEND-":
             webhook = values["-WEBHOOK-"]
             key = values["-KEY-"]
@@ -150,28 +246,28 @@ def main():
                 file_path, file_extension = os.path.splitext(filepath)
                 file_size = os.path.getsize(filepath)
                 file_size_kb = file_size / 1024
-                print(f'\nFilesize is {file_size_kb:.2f} Kilobytes')
+                print(f"\nFilesize is {file_size_kb:.2f} Kilobytes")
                 allowed_file_extensions = [".png", ".jpg", ".jpeg"]
                 if file_size <= 1048576 and file_extension in allowed_file_extensions:
-                    Grouplotse.webhook_file_post(Grouplotse, webhook, key, filepath, file_extension)
-                    print(f'\nSending a {file_extension} image to GroupLotse')
-                    window.find_element('-FILEPATHWH-').Update('')
+                    Grouplotse.webhook_file_post(
+                        Grouplotse, webhook, key, filepath, file_extension
+                    )
+                    print(f"\nSending a {file_extension} image to GroupLotse")
+                    window.find_element("-FILEPATHWH-").Update("")
                 if file_size >= 1048576:
                     print("\nFile is too large! Max File Size is 1024Kb / 1 MegaByte.")
                 if file_extension not in allowed_file_extensions:
                     print("\nFiletype must be a .png or .jpg/jpeg\n")
-
 
             with open("grouplotse_tester_webhook.txt", "w") as glsetwebhook:
                 glsetwebhook.write(webhook + "#" + key)  # append data
                 print(f"Saved {webhook}#{key} to list")
                 glsetwebhook.close()
 
-
         if event == "-MQTTSEND-":
             broker = values["-MQTTBROKER-"]
             port_string = values["-MQTTPORT-"]
-            port = (int(port_string))
+            port = int(port_string)
             topic = values["-MQTTTOPIC-"]
             username_unf = values["-MQTTUSERNAME-"]
             username = str(username_unf)
@@ -182,15 +278,23 @@ def main():
             Grouplotse.mqtt_send(Grouplotse, topic, username, password, mqttmsg)
 
             with open("grouplotse_tester_mqtt.txt", "w") as glsetmqtt:
-                glsetmqtt.write("http://mqtt.grouplotse.com#1883"+"#"+topic+"#"+username+"#"+password)
+                glsetmqtt.write(
+                    "http://mqtt.grouplotse.com#1883"
+                    + "#"
+                    + topic
+                    + "#"
+                    + username
+                    + "#"
+                    + password
+                )
                 print(f"Saved {topic}#{username}#{password} to list")
                 glsetmqtt.close()
 
         if event == "-WEBSITEBUTTON-":
             webbrowser.open("https://grouplotse.com/")
 
-
     window.close()
+
 
 if __name__ == "__main__":
     main()
